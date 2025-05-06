caa422f4738367fd6f1bc56cbd55535b243e4e41
william77910 <williamfaucomprez@gmail.com>
2025-05-06 17:17:10 +0200

    Ajout des pages d'authentification et mise à jour des routes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),];
+    new Route("/signin", "Connexion", "/pages/authentification/signin.html"),
+    new Route("/signup", "Inscription", "/pages/authentification/signup.html"),
+    new Route("/account", "Mon compte", "/pages/authentification/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/authentification/editPassword.html"),];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
