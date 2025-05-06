81ceda841e2c6fc63bc1c3481f617324a98dcbe6
william77910 <williamfaucomprez@gmail.com>
2025-05-06 14:32:01 +0200

    Ajout de la page carte et mise à jour des liens de navigation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,7 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),];
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/carte", "La carte", "/pages/carte.html"),];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
