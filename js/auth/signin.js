e1676a8d77717a4260ebafbd7cdc2b986bedf49b
william77910 <williamfaucomprez@gmail.com>
2025-05-23 19:03:54 +0200

    Amélioration de la lisibilité du code dans la fonction de connexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,8 +1,8 @@
-const mailInput = document.getElementById("EmailInput");  // Récupérer l'input email
-const passwordInput = document.getElementById("PasswordInput");  // Récupérer l'input password
-const btnSignin = document.getElementById("btnSignin");  // Récupérer le bouton de connexion
+const mailInput = document.getElementById("EmailInput");            // Récupérer l'input email
+const passwordInput = document.getElementById("PasswordInput");     // Récupérer l'input password
+const btnSignin = document.getElementById("btnSignin");             // Récupérer le bouton de connexion
 
-btnSignin.addEventListener("click", checkCredentials);  // Ajouter un écouteur d'événement au bouton de connexion
+btnSignin.addEventListener("click", checkCredentials);              // Ajouter un écouteur d'événement au bouton de connexion
 // Fonction pour vérifier les identifiants de l'utilisateur
 
 function checkCredentials(){
@@ -15,14 +15,14 @@ function checkCredentials(){
         // Ici, vous pouvez stocker le token JWT dans le localStorage ou le sessionStorage
         // il faudra récupérer le vrai token
         const token = "mvorjavoja^vohjvôirvjn^rovn"
-        setToken(token);  // Appeler la fonction pour stocker le token
+        setToken(token);                                           // Appeler la fonction pour stocker le token
         // placer ce token en cookie
 
-        setCookie(RoleCookieName, "admin", 7); // 7 jours de validité
-        window.location.replace("/"); // Rediriger vers la page d'accueil
+        setCookie(RoleCookieName, "admin", 7);                     // 7 jours de validité
+        window.location.replace("/");                              // Rediriger vers la page d'accueil
     }
     else{
-        mailInput.classList.add("is-invalid");  // Ajouter la classe is-invalid à l'input email
-        passwordInput.classList.add("is-invalid");  // Ajouter la classe is-invalid à l'input password
+        mailInput.classList.add("is-invalid");                    // Ajouter la classe is-invalid à l'input email
+        passwordInput.classList.add("is-invalid");                // Ajouter la classe is-invalid à l'input password
     }
 }
